bc4553261079c62c1d79d08f61278688e6f2c7d0
Ismael Marques <ismaelhmarques@hotmail.com>
2022-08-20 16:37:08 -0300

    primeiro commit

---
+++ AtividadePizzaria-main/AtividadePizzaria/Data/InicializadorDeDados.cs
@@ -0,0 +1,69 @@
+﻿using AtividadePizzariaIsmael.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtividadePizzariaIsmael.Data
+{
+    public class InicializadorDeDados
+    {
+        public static void Inicializar(IApplicationBuilder builder)
+        {
+            using IServiceScope serviceScope = builder.ApplicationServices.CreateScope();
+            var context = serviceScope
+                .ServiceProvider
+                .GetService<PizzariaDbContext>();
+
+            context.Database.EnsureCreated();
+
+            if (!context.Sabores.Any())
+            {
+                context.Sabores.AddRange(new List<Sabor>()
+                    {
+                        new Sabor("Massa Tradicional", "https://maringa.cidadecancao.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/m/a/massa-pizza-tradicional-kg-0000000025263.jpg"),
+                        new Sabor("Atum", "https://oreinodopeixe.com.br/wp-content/uploads/2019/08/NOVO_Atum2.jpg"),
+                        new Sabor("Calabresa", "https://oacougue.com.br/wp-content/uploads/2020/06/LINGUICA-CALABRESA-DEFUMADA.jpg"),
+                        new Sabor("Mussarela", "https://static.clubeextra.com.br/img/uploads/1/0/583000.jpg")
+                    });
+                context.SaveChanges();
+            }
+
+            if (!context.Tamanhos.Any())
+            {
+                context.Tamanhos.AddRange(new List<Tamanho>()
+                    {
+                        new Tamanho("Pequena"),
+                        new Tamanho("Média"),
+                        new Tamanho("Grande")
+                    });
+                context.SaveChanges();
+            }
+
+            if (!context.Pizzas.Any())
+            {
+                context.Pizzas.AddRange(new List<Pizza>()
+                    {
+                        new Pizza("Argentina",30,"https://media.istockphoto.com/photos/pizza-argentina-picture-id1047708734",1),
+                        new Pizza("Cinco Queijos",40,"http://luccapizzaria.com.br/wp-content/uploads/2020/04/5queijos.jpg",2),
+                        new Pizza("Atum I",50,"https://www.comidaereceitas.com.br/img/sizeswp/1200x675/2015/03/pizza_atum_especial.jpg",3)
+                    });
+                context.SaveChanges();
+            }
+
+            if (!context.PizzasSabores.Any())
+            {
+                context.PizzasSabores.AddRange(new List<PizzaSabor>()
+                    {
+                        new PizzaSabor(1,3),
+                        new PizzaSabor(2,4),
+                        new PizzaSabor(3,2),
+                        new PizzaSabor(1,1),
+                        new PizzaSabor(2,1),
+                        new PizzaSabor(3,1)
+                    });
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ AtividadePizzaria-main/AtividadePizzaria/Models/ViewModels/RequestDTO/PostPizzaVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtividadePizzariaIsmael.Models.ViewModels.RequestDTO
+{
+    public class PostPizzaVM
+    {
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "O Nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Preço")]
+        [Required(ErrorMessage = "O Preço é obrigatório!")]
+        public decimal Preco { get; set; }
+
+        [Display(Name = "Imagem")]
+        [Required(ErrorMessage = "A imagem é obrigatóra!")]
+        public string FotoURL { get; set; }
+
+        [Display(Name = "Informe os Sabores")]
+        [Required(ErrorMessage = "Sabores é Obrigatório")]
+        public List<int> SaboresId { get; set; }
+
+
+        [Display(Name = "Informe o Tamanho")]
+        [Required(ErrorMessage = "Tamanho é Obrigatório")]
+        public int TamanhoId { get; set; }
+
+    }
+}
